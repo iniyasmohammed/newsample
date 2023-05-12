9f186c3b75b76ca713de33db4cc064c5cec2861c
iniyasmohammed <iniyasmohammed@gmail.com>
2023-05-12 22:54:21 +0300

    assignment number:1 finished

---
+++ script.js
@@ -0,0 +1,53 @@
+// addition
+let x = 23;
+let y = 34;
+let z = x+y;
+
+console.log(z);
+//substraction
+
+let a = 55;
+let b = 36;
+let c = a-b;
+
+console.log(c);
+// multiplication
+let p = 34;
+let q = 25;
+let r = p*q;
+
+console.log(r);
+division
+let x1 = 15;
+let x2 = 5;
+console.log(x1/x2);
+
+// mode
+let y1 = 17;
+let y2 = 3;
+let yy = y1%y2;
+
+console.log(yy);
+//multiple operators
+let a1 = 45;
+let b1 = 34;
+let c1 = 23;
+let d1 = 3;
+
+let e = a1+b1*c1-d1;
+
+console.log(e);
+
+//declaring results using 'if...else' condition
+let sub1 = 70;
+let sub2 = 80;
+let sub3 = 90;
+let sub4 = 50;
+
+let sum = sub1+sub2+sub3+sub4;
+
+if(sum>250){
+    console.log("congradulations, you are passed");
+}else{
+    console.log("better luck next time");
+}
